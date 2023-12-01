2567f93109da3b7489465a3cff08f7cb334223fb
Frédéric Massart <fred@branchup.tech>
2023-12-01 13:04:20 +0800

    Release v1.5.1

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023120100;
+$plugin->version   = 2023120101;
 $plugin->requires  = 2020061500;    // Moodle 3.9.0.
 $plugin->component = 'mod_teammeeting';
-$plugin->release   = 'v1.5.0';
+$plugin->release   = 'v1.5.1';
 $plugin->maturity  = MATURITY_STABLE;
