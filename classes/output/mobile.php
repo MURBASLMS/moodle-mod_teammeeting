8c4493e6de7158a694c52e81c27b7605555d4e8d
Frédéric Massart <fred@branchup.tech>
2025-01-20 10:01:47 +0800

    Mobile function failed due to missing externallib import

--- classes/output/mobile.php
+++ classes/output/mobile.php
@@ -25,6 +25,7 @@ namespace mod_teammeeting\output;
 
 defined('MOODLE_INTERNAL') || die();
 
+require_once($CFG->libdir . '/externallib.php');
 require_once($CFG->dirroot . '/mod/teammeeting/lib.php');
 
 /**
